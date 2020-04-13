77852557873d2deaf2d427c723fa50a7b24b25f5
wen <waynevirus1983@gmail.com>
2020-04-13 10:49:57 +0800

    123

---
+++ GoogleMap/1.js
@@ -0,0 +1,5 @@
+{
+    "type":"FeatureCollection",
+    "features":
+    [{"type":"Feature",
+    "geometry":{"type":"Point","coordinates":[121.521188,23.601927]},"properties":{"id":"0145080011","name":"衛生福利部花蓮醫院豐濱原住民分院","masksLeft":0,"childMasksLeft":12}}
