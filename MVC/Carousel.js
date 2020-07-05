cab3f21b7c882ac799a96a77985b15fd9c1046ff
wen <waynevirus1983@gmail.com>
2020-07-05 13:44:57 +0800

    123

---
+++ MVC/Carousel.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.multiple-items').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrow: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        centerMode: true,
+        
+    });
+})
+
+
